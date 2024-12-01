aec0b59293d5113e129b08feb89ffa101ba038e4
Anastasia_S <sivokonanastasia1@gmail.com>
2024-12-01 23:52:28 +0200

    ain

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,11 @@
-const flatpickr = require("flatpickr");
+// Описаний в документації
 import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
 
 const startBtn = document.querySelector('[data-start]');
 const dateTimePicker = document.querySelector('#datetime-picker');
